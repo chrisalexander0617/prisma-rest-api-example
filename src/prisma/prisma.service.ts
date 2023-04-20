2fa3c6df9cbeba8fa5517e8726fad9c8e9756a15
Chris <chrisalexander0617@gmail.com>
2023-04-20 10:52:36 -0400

    init commit

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,13 @@
+import { INestApplication, Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  
+  // this ensures the app shuts down gracefully
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
